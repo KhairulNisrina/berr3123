1fa368ebd2a02ff95bc6ad0ba4cf94b21d885097
KhairulNisrina <B022210192@student.utem.edu.my>
2025-01-20 21:34:48 +0800

    commit

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ app.post('/api/users/register', async (req, res) => {
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 requests per windowMs
+  max: 3, // Limit each IP to 5 requests per windowMs
   message: 'Too many login attempts from this IP, please try again later',
 });
 
